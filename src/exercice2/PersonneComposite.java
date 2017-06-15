5ce8f96158277ef2cbeccee9cfead71b1c6226f1
Aurélien Rouzé <aurelien.rouze@gmail.com>
2017-06-15 16:37:44 +0200

    Tchoin

---
+++ src/exercice2/PersonneComposite.java
@@ -0,0 +1,28 @@
+package exercice2;
+
+import java.util.ArrayList;
+
+public class PersonneComposite implements Personne{
+	
+	private ArrayList<Personne> ListPersonne = new ArrayList<Personne>();
+	
+	public void add(Personne personne)  {
+		ListPersonne.add(personne);
+	}
+	
+	public void remove(Personne personne)  {
+		ListPersonne.remove(personne);
+	}
+
+	@Override
+	public void print() {
+		for (Personne personne : ListPersonne) {
+			personne.print();
+		}
+		
+	}
+
+	
+	//Add Remove Delete Se marier
+
+}
